a9a6cb26a34e3c420adfadf328ce3ae10acca170
Franco D'Agostino <144281050+Francodago@users.noreply.github.com>
2025-05-28 15:23:29 -0400

    The dashboard is finished

--- main.js
+++ main.js
@@ -24,3 +24,22 @@ new Chart(chart,{
         responsive:true
     }
 });
+
+const menuBtn =document.querySelector("#menu-btn");
+const closeBtn =document.querySelector("#close-btn");
+const sidebar =document.querySelector("aside");
+menuBtn.addEventListener("click",()=>{
+    sidebar.classList.add("open");
+    document.body.style.overflow = "hidden";
+});
+closeBtn.addEventListener("click", ()=>{
+    sidebar.classList.remove("open");
+    document.body.style.overflow = "auto";
+});
+
+const themeBtn=document.querySelector(".theme-btn");
+themeBtn.addEventListener("click", ()=>{
+    document.body.classList.toggle("dark-theme");
+    themeBtn.querySelector('span:first-child').classList.toggle('active');
+    themeBtn.querySelector('span:last-child').classList.toggle('active');
+});
